2596e13a7898e426069370fe7fcb3fdb24287bbe
s01ver <solverglace@gmail.com>
2023-01-27 10:18:59 +0900

    Initial Commit

---
+++ ToDoList/Model/TaskFilter.swift
@@ -0,0 +1,21 @@
+//
+//  TaskFilter.swift
+//  ToDoList
+//
+//
+//
+
+import SwiftUI
+
+enum TaskFilter: String
+{
+    static var allFilters: [TaskFilter]
+    {
+        return [.NonCompleted,.Completed,.OverDue,.All]
+    }
+    
+    case All = "All"
+    case NonCompleted = "To Do"
+    case Completed = "Completed"
+    case OverDue = "Overdue"
+}
---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            
+            let context = persistenceController.container.viewContext
+            let dateHolder = DateHolder(context)
+            
+            TaskListView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(dateHolder)
+        }
+    }
+}
